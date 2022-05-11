994a6e9add787bfb4c1b9ab82083b63ee8d2ca5b
Serva <servas230@gmail.com>
2022-05-11 08:55:42 +0200

    Modified scoreView

--- src/views/scoreView.js
+++ src/views/scoreView.js
@@ -9,26 +9,11 @@
 
 export const createScoreElement = () => {
   const element = document.createElement('div');
-  element.className = 'score';
 
-  element.innerHTML = 'hello';
-
-  /*  element.innerHTML = String.raw`
+  element.innerHTML = String.raw`
   score = ${scoreCounter} of 10;
-`; */
+`;
   return element;
 };
 
 createScoreElement();
-
-/*const scoreCounter = (){
-  let score = 0; 
-const correctAnswer = currentQuestion.correct;
-const selectedAnswer = document.getElementBy...('');
-if (selectedAnswer === correctAnswer) {
- return  score++;
-} else {
- return score;
-}
-
-} */

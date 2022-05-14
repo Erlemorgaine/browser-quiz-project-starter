4269d5c794dd8d83cc44bd3879d545b9278a09ed
ysmnclsknnl <101097614+ysmnclsknnl@users.noreply.github.com>
2022-05-14 19:29:38 +0200

    all functionality is done

--- src/app.js
+++ src/app.js
@@ -1,27 +1,27 @@
 'use strict';
 
 import { quizData } from './data.js';
-import { initQuestionPage } from './pages/questionPage.js';
+import { initQuestionPage, updateScore } from './pages/questionPage.js';
 import { initWelcomePage } from './pages/welcomePage.js';
 import { initFinalPage } from './pages/finalPage.js';
 
 const loadApp = () => {
   if (localStorage.getItem('selectedAnswersArray')) {
+    const amountOfQuestions = quizData.questions.length;
+
     const selectedAnswers = localStorage.getItem('selectedAnswersArray');
     const selectedAnswersArray = JSON.parse(selectedAnswers);
     const amountOfQuestionAnswered = selectedAnswersArray.length;
 
-    if (amountOfQuestionAnswered === quizData.questions.length) {
-      // const currentScore = quizData.questions.filter(
-      //   (question) => question.correct === question.selected
-      // );
-      initFinalPage();
-      return;
+    if (amountOfQuestionAnswered === amountOfQuestions) {
+      const currentScore = updateScore(quizData.questions);
+      initFinalPage(currentScore, amountOfQuestions);
     } else {
       quizData.currentQuestionIndex = amountOfQuestionAnswered;
       for (let i = 0; i < amountOfQuestionAnswered; i++) {
         quizData.questions[i].selected = selectedAnswersArray[i];
       }
+
       initQuestionPage();
     }
   } else {

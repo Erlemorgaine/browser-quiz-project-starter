8e4047db4bb3e47c23f2557bd8f2ba3d45cb6a20
ysmnclsknnl <101097614+ysmnclsknnl@users.noreply.github.com>
2022-05-14 19:30:17 +0200

    two parameters are defined  for function

--- src/pages/finalPage.js
+++ src/pages/finalPage.js
@@ -3,15 +3,15 @@
 import { USER_INTERFACE_ID, CURRENT_SCORE_ID } from '../constants.js';
 import { createClosingElement } from '../views/finalPageView.js';
 
-export const initFinalPage = () => {
+export const initFinalPage = (currentScore, amountOfQuestions) => {
   const userInterface = document.getElementById(USER_INTERFACE_ID);
   userInterface.innerHTML = '';
 
   const closingElement = createClosingElement();
   userInterface.appendChild(closingElement);
 
-  //   closingElement.innerHTML = String.raw`
-  //   <p> All done!<br> Your final score is : <span id='${CURRENT_SCORE_ID}'>${currentScore}</span> of ${quizData.questions.length} </p>
-  //  `;
+  closingElement.innerHTML = String.raw`
+    <p> All done!<br> Your final score is : <span id='${CURRENT_SCORE_ID}'>${currentScore}</span> of ${amountOfQuestions} </p>
+   `;
   localStorage.removeItem('selectedAnswersArray');
 };

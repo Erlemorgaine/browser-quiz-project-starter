c6e15df9b688f75172cc8c14a929b135989caf54
ysmnclsknnl <101097614+ysmnclsknnl@users.noreply.github.com>
2022-05-16 14:19:06 +0200

    selected answers and correct answers are dislayed on page

--- src/pages/finalPage.js
+++ src/pages/finalPage.js
@@ -1,17 +1,29 @@
 'use strict';
-
+import { quizData } from '../data.js';
 import { USER_INTERFACE_ID, CURRENT_SCORE_ID } from '../constants.js';
 import { createClosingElement } from '../views/finalPageView.js';
 
 export const initFinalPage = (currentScore, amountOfQuestions) => {
+  const backgroundEl = document.getElementById('background');
+  backgroundEl.classList.add('background-question-page');
+
   const userInterface = document.getElementById(USER_INTERFACE_ID);
   userInterface.innerHTML = '';
 
-  const closingElement = createClosingElement();
+  const closingElement = createClosingElement(currentScore, amountOfQuestions);
   userInterface.appendChild(closingElement);
 
-  closingElement.innerHTML = String.raw`
-    <p> All done!<br> Your final score is : <span id='${CURRENT_SCORE_ID}'>${currentScore}</span> of ${amountOfQuestions} </p>
-   `;
+  const questions = quizData.questions;
+  questions.forEach((question) => {
+    const selectedAnswer = question.answers[question.selected]
+      ? question.answers[question.selected]
+      : 'Not replied';
+    const correctAnswer = question.answers[question.correct];
+    // const indexOfCorrectAnswer = getTheIndexOfAnswer(question.correct);
+    closingElement.innerHTML += `<div class='result'><p class='question'>${question.text} <p>
+    <p>Selected: ${selectedAnswer}</p>
+    <p>Correct:  ${correctAnswer}</p></div>`;
+  });
+
   localStorage.removeItem('selectedAnswersArray');
 };

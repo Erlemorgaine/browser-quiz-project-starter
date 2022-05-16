731bf6d4af277d68b5075e5d2d0b9d64add8cf19
ysmnclsknnl <101097614+ysmnclsknnl@users.noreply.github.com>
2022-05-16 14:20:17 +0200

    scrollable class is added to finalpae view

--- src/views/finalPageView.js
+++ src/views/finalPageView.js
@@ -1,17 +1,18 @@
 'use strict';
-
+import { CURRENT_SCORE_ID } from '../constants.js';
 /**
  * Create a closing element
  * @returns {Element}
  */
 
-export const createClosingElement = () => {
+export const createClosingElement = (currentScore, amountOfQuestions) => {
   const element = document.createElement('div');
-  element.className = 'final-page';
+  element.classList.add('final-page');
+  element.classList.add('scrollable');
 
   element.innerHTML = String.raw`
- <p>  </p>
-`;
+  <h1> All done!<br> Your final score is : <span id='${CURRENT_SCORE_ID}'>${currentScore}</span> of ${amountOfQuestions} </h1>
+ `;
 
   return element;
 };
